19caeae350d6c41a1ff6158b37821366cb67077f
ffmaer <ffmaer@gmail.com>
2013-09-26 16:13:48 -0400

    code

---
+++ LinkedList/List-Flattening.c
@@ -0,0 +1,8 @@
+// This codes tries to flatten a list with three pointers, namely, next, prev and child.
+// It is like a tree, so the tree traversal approach can work.
+// But the algorithm here is not recursive and it uses less memory.
+// The complexity is O(2n), or O(n).
+
+void flattenList(Node *head, Node **tail){
+	
+}
